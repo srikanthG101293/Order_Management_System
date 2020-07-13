98802b7931f7f7376b11528e8c832740f3bb96ed
Sriknth <srikanth101293@gmail.com>
2020-07-13 10:15:18 +0530

    intial changes

---
+++ OrderManagement/Models/ProdutEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OrderManagement.Models
+{
+	public class ProdutEntity
+	{
+        public int OrderID { get; set; }
+        public string UserName { get; set; }
+        public string ProductName { get; set; }
+        public string ShippingAddress { get; set; }
+        public string OrderStatus { get; set; }
+    }
+}
